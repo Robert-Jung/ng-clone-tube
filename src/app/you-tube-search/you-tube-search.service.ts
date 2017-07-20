d847fea5d71c866b442252bf9534a01ba5dc5b0e
Robert-Jung <rjinjung@gmail.com>
2017-07-19 19:19:53 -0700

    Adding youtube search function to return an array of search results.

--- src/app/you-tube-search/you-tube-search.service.ts
+++ src/app/you-tube-search/you-tube-search.service.ts
@@ -1,5 +1,10 @@
 import { Injectable, Inject } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+export const YOUTUBE_API_KEY = 'AIzaSyDOfT_BO81aEZScosfTYMruJobmpjqNeEk';
+export const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 @Injectable()
 export class YouTubeSearchService {
@@ -7,7 +12,26 @@ export class YouTubeSearchService {
     @Inject(YOUTUBE_API_KEY) private apiKey: string,
     @Inject(YOUTUBE_API_URL) private apiUrl: string) {
     }
-}
 
-export const YOUTUBE_API_KEY = 'AIzaSyDOfT_BO81aEZScosfTYMruJobmpjqNeEk';
-export const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/search';
+    search(query: string): Observable<SearchResult[]> {
+    const params: string = [
+        `q=${query}`,
+        `key=${this.apiKey}`,
+        `part=snippet`,
+        `type=video`,
+        `maxResults=10`
+    ].join('&');
+    const queryUrl = `${this.apiUrl}?${params}`;
+    return this.http.get(queryUrl)
+    .map((response: Response) => {
+        return(<any>response.json()).items.map(item => {
+            return new SearchResult({
+                id: item.id.videoId,
+                title: item.snippet.title,
+                description: item.snippet.description,
+                thumbnailUrl: item.snippet.thumbnails.high.url
+            });
+        });
+    });
+    }
+}
